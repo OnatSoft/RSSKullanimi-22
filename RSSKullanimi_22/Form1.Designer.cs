9e9848eec10d9bf6bab2035c0bde199982cf3ea4
OnatSoft <onat.somer2018@gmail.com>
2023-10-15 01:21:41 +0300

    Add project files.

---
+++ RSSKullanimi_22/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace RSSKullanimi_22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_SozcuHaber = new System.Windows.Forms.Button();
+            this.btn_MynetHaber = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_SozcuHaber
+            // 
+            this.btn_SozcuHaber.BackColor = System.Drawing.Color.OrangeRed;
+            this.btn_SozcuHaber.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_SozcuHaber.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SozcuHaber.Location = new System.Drawing.Point(100, 12);
+            this.btn_SozcuHaber.Name = "btn_SozcuHaber";
+            this.btn_SozcuHaber.Size = new System.Drawing.Size(183, 44);
+            this.btn_SozcuHaber.TabIndex = 0;
+            this.btn_SozcuHaber.Text = "www.cumhuriyet.com.tr";
+            this.btn_SozcuHaber.UseVisualStyleBackColor = false;
+            this.btn_SozcuHaber.Click += new System.EventHandler(this.btn_SozcuHaber_Click);
+            // 
+            // btn_MynetHaber
+            // 
+            this.btn_MynetHaber.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btn_MynetHaber.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_MynetHaber.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_MynetHaber.Location = new System.Drawing.Point(458, 12);
+            this.btn_MynetHaber.Name = "btn_MynetHaber";
+            this.btn_MynetHaber.Size = new System.Drawing.Size(168, 44);
+            this.btn_MynetHaber.TabIndex = 1;
+            this.btn_MynetHaber.Text = "www.mynet.com";
+            this.btn_MynetHaber.UseVisualStyleBackColor = false;
+            this.btn_MynetHaber.Click += new System.EventHandler(this.btn_MynetHaber_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(100, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(484, 78);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(111, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(34, 158);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(314, 429);
+            this.panel1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(314, 429);
+            this.listBox1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Location = new System.Drawing.Point(381, 158);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(314, 429);
+            this.panel2.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 17;
+            this.listBox2.Location = new System.Drawing.Point(0, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(314, 429);
+            this.listBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(96, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Gündem Haberleri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(440, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Son Dakika Haberleri";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(717, 612);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_MynetHaber);
+            this.Controls.Add(this.btn_SozcuHaber);
+            this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_SozcuHaber;
+        private System.Windows.Forms.Button btn_MynetHaber;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RSSKullanimi_22/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RSSKullanimi_22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SozcuHaber_Click(object sender, EventArgs e)
+        {
+            XmlTextReader xmloku = new XmlTextReader("https://www.cumhuriyet.com.tr/rss");
+
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString().ToString());
+                }
+            }
+        }
+
+        private void btn_MynetHaber_Click(object sender, EventArgs e)
+        {
+            XmlTextReader xmloku2 = new XmlTextReader("https://www.mynet.com/haber/rss/sondakika");
+
+            while (xmloku2.Read())
+            {
+                if (xmloku2.Name == "title")
+                {
+                    listBox2.Items.Add(xmloku2.ReadString());
+                }
+            }
+        }
+    }
+}
